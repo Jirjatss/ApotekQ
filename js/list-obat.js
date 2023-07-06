f2dcfd2915c76ef8c354b5420ec37b1a02e532a8
agha david <aghadvid@gmail.com>
2023-07-06 14:09:32 +0700

    list obat

---
+++ js/list-obat.js
@@ -0,0 +1,67 @@
+const obat = [
+    {nama : `kalpanax`,
+    type : `salep`,
+    kategori :`gangguan kulit`,
+    harga : 7500
+    },
+    {nama : `paratusin`,
+    type :`tablet`,
+    kategori :`Antiseptik`,
+    harga : 1700
+    },
+    {nama : `Alpara`,
+    type :`tablet`,
+    kategori :`batuk & pilek`,
+    harga : 9000
+    },
+    {nama : `demacolin`,
+    type : `tablet`,
+    kategori :`batuk & pilek`,
+    harga : 7000
+    },
+    {nama : `konidin`,
+    type :`tablet`,
+    kategori :`batuk & pilek`,
+    harga : 3000
+    },
+    {nama : `Polysilane`,
+    type :`tablet`,
+    kategori :`gangguan pencernaan`,
+    harga : 10300
+    },
+    {nama : `promag`,
+    type :`tablet`,
+    kategori :`gangguan pencernaan`,
+    harga : 8700
+    },
+    {nama : `herocyn`,
+    type :`powder`,
+    kategori :`gangguan kulit`,
+    harga : 16000
+    },
+    {nama : `salep 88`,
+    type :`salep`,
+    kategori :`gangguan kulit`,
+    harga : 13000
+    },
+    {nama : `bedak salicyl`,
+    type :`powder`,
+    kategori :`gangguan kulit`,
+    harga : 8.700
+    },
+    {nama : `sanmol`,
+    type :`tablet`,
+    kategori :`pusing & demam`,
+    harga : 2200
+    },
+    {nama : `decolgen`,
+    type :`tablet`,
+    kategori :`pusing & demam`,
+    harga : 2500
+    },
+    {nama : `panadol biru`,
+    type :`tablet`,
+    kategori :`pusing & demam`,
+    harga : 12.500
+    },
+]
